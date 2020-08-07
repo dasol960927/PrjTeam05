a698152d7b8f644da199e4fee21a7a25c3cbe637
ssongji93 <ssongji93@naver.com>
2020-08-07 14:48:35 +0900

    2020_08_07 14:48 쏭지 수정

--- Team5/src/main/java/com/spring/board/service/BoardService.java
+++ Team5/src/main/java/com/spring/board/service/BoardService.java
@@ -1,11 +1,12 @@
 package com.spring.board.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import com.spring.board.vo.BoardVo;
 
 public interface BoardService {
 
-	List<BoardVo> getBoardList(String gubun);
+	List<BoardVo> getBoardList(HashMap<String, Object> map);
 
 }
